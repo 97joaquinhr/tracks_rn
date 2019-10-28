578160f05c96c864d253eb00bbd9d91ba3884247
97joaquinhr <97joaquinhr@gmail.com>
2019-10-28 13:44:25 -0600

    Signup working

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,64 @@
-import React from 'react';
-import {View,Text,StyleSheet,Button} from 'react-native';
+import React,{useState,useContext} from 'react';
+import {View,StyleSheet} from 'react-native';
+import {Text,Input, Button} from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import {Context as AuthContext} from '../context/AuthContext';
+
 const SignupScreen = ({navigation})=>{
-    return <>
-        <Text style={{fontSize:48}}>SignupScreen</Text>
-        <Button title ="Go to Signin" onPress={()=> navigation.navigate('Signin')}/>
-        <Button title ="Go to main flow" onPress={()=> navigation.navigate('mainFlow')}/>
-    </>
+    const {state,signup} = useContext(AuthContext);
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+
+    return (
+        <View style = {styles.container}>
+            <Spacer>
+                <Text h3>Sign up for Tracker</Text>
+            </Spacer>
+            
+            <Input 
+                label = "Email"
+                value = {email}
+                onChangeText = {setEmail}
+                autoCapitalize="none"
+                autoCorrect = {false}
+            />
+            <Spacer/>
+            <Input 
+                label="Password"
+                value = {password}
+                onChangeText = {setPassword}
+                autoCapitalize="none"
+                autoCorrect = {false}
+                secureTextEntry
+            />
+            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text>:null}
+            <Spacer>
+                <Button title="Sign up" onPress={()=>{signup({email,password})}}/>
+            </Spacer>
+            
+        </View >
+    );
 };
-const styles = StyleSheet.create({});
+
+SignupScreen.navigationOptions = () =>{
+    return {
+        header: null,
+    };
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        marginBottom: 200
+    },
+    errorMessage:{
+        fontSize:16,
+        color: 'red',
+        marginLeft:15,
+        marginTop:15,
+    }
+});
+
 export default SignupScreen;
