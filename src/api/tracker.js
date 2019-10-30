2e32c276f672a497f09048b237343fe1a4109608
97joaquinhr <97joaquinhr@gmail.com>
2019-10-29 20:03:01 -0600

    Done with tracks app

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import {AsyncStorage} from 'react-native';
 
-export default axios.create({
-    baseURL: 'http://b37916b7.ngrok.io'
+const instance =axios.create({
+    baseURL: 'http://06309a12.ngrok.io'
 });
+
+instance.interceptors.request.use(
+    async (config)=>{
+        const token = await AsyncStorage.getItem('token');
+        if (token){
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err)=>{
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,22 +1,37 @@
 import '../_mockLocation';
-import React, {useContext} from 'react';
+import React, {useContext,useCallback} from 'react';
 import {StyleSheet} from 'react-native';
 import MapT from '../components/MapT';
 import {SafeAreaView,withNavigationFocus} from 'react-navigation';
 import {Text} from 'react-native-elements';
 import useLocation from '../hooks/useLocation';
 import {Context as LocationContext} from '../context/LocationContext';
+import TrackForm from '../components/TrackForm';
+import {FontAwesome} from '@expo/vector-icons';
 
 const TrackCreateScreen = ({isFocused})=>{
-    const {addLocation} =  useContext(LocationContext);
-    const [err] = useLocation(isFocused,addLocation);
+    const {state:{recording}, addLocation} =  useContext(LocationContext);
+    const callback = useCallback(
+        location=>{
+            addLocation(location,recording);
+        },
+        [recording]
+    );
+    const [err] = useLocation(isFocused||recording,callback);
+    
     return( 
         <SafeAreaView forceInset={{top:'always'}}>
             <Text h3>Create a Track</Text>
             <MapT/>
             {err? <Text>Please enable location services</Text>:null}
+            <TrackForm/>
         </SafeAreaView>        
     );
+};
+
+TrackCreateScreen.navigationOptions ={
+    title: 'Add track',
+    tabBarIcon: <FontAwesome name="plus" size={20}/>
 };
 
 const styles = StyleSheet.create({});
